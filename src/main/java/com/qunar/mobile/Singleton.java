cb6337bdcc311903526111b4c8d89c28283b0d0f
jinhua.zhang <jinhua.zhang@qunar.com>
2017-04-04 15:53:01 +0800

    singleton

---
+++ src/main/java/com/qunar/mobile/Singleton.java
@@ -0,0 +1,21 @@
+package com.qunar.mobile;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * 单例模式
+ * <p>
+ * Created by jinhua on 17-4-4.
+ */
+public class Singleton {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<Runtime> runtimeClass = Runtime.class;
+
+        Constructor<Runtime> runtimeConstructor = runtimeClass.getDeclaredConstructor();
+
+        runtimeConstructor.setAccessible(true);
+
+        System.out.println(Runtime.getRuntime() == runtimeConstructor.newInstance());
+    }
+}
